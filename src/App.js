00d7f803bdf2edb59bb744441835f63cc9966e16
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-01-22 15:48:37 -0300

    commit init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [message, setMessage] = useState('Hello World!')
+
+    return (
+        <>
+            <div className="container">
+                <h1>{message}</h1>
+            </div>
+
+            <button onClick={() => setMessage('Helooo diferente')}>Mudar Mensagem</button>
+        </>
+    );
 }
 
-export default App;
+export default App
