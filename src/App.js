d4aed040fd00f6485d148d2567dce935a1c2534f
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-01-23 09:12:24 -0300

    Add useEffect and axios for fetching tasks from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import './App.css';
 import Tasks from './components/Tasks'
@@ -9,10 +10,23 @@ import TaskDetails from './components/TaskDetails';
 
 
 const App = () => {
-    const [ tasks, setTasks] = useState([
+    /* const [ tasks, setTasks] = useState([
         { id: '1', title: 'Estudar programação', completed: false },
         { id: '2', title: 'Ler livros', completed: false },
-    ])
+    ]) */
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const { data } = await axios.get(
+                'https://jsonplaceholder.cypress.io/todos?_limit=5'
+            )
+
+            setTasks(data)
+        }
+
+        fetchTasks()
+    
+    }, [])
 
     const handleTaskAddition = (taskTitle) => {
         const newTasks = [
